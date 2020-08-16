76d96d53d953572ef6614f451a83f2073a287a21
platina78 <ghdtjdwns87@gmail.com>
2020-08-16 12:27:48 +0900

    Add Schedule

--- lib/main.dart
+++ lib/main.dart
@@ -68,25 +68,33 @@ class _NotificationState extends State<Notification> {
         .initialize(initializationSettings); //세팅 값 설정
   }
 
+  
+
   Future _showNotification() async {
     //Detail에는 icon이나 push 알람이 일어났을 때의 알람소리등의 디테일 부분을 설정
     var androidPlatformChannelSpecifics = AndroidNotificationDetails(
       '앱 아이디?',
       '앱 이름',
       '앱의 주소',
+      importance: Importance.Max,
+      priority: Priority.High
     );
 
     var iosPlatformChannelSpecifics = IOSNotificationDetails();
     var platformChannelSpecifics = NotificationDetails(
         androidPlatformChannelSpecifics, iosPlatformChannelSpecifics);
+    var scheduledNotificationDateTime =
+        DateTime.now().add(Duration(seconds: 5));
 
-    await _flutterLocalNotificationsPlugin.show(
+    await _flutterLocalNotificationsPlugin.schedule(
       0, //해당 notification의 id를 나타내며 이 id값을 통해 Notication을 취소한다.
       'Notification 제목',
       'Notification 내용',
+      scheduledNotificationDateTime,
       platformChannelSpecifics,
       payload: 'Notification Test',
     );
+
   }
 
   @override
